f1aa6ddf28454d8c31d7b8f17cdccfb518d860ed
Tin <tinnystudios@gmail.com>
2020-02-16 16:03:39 +1100

    Add some notes to the server spawner

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,18 +1,41 @@
-﻿using System.Collections;
+﻿using Unity.Collections;
+using Unity.Networking.Transport;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    private IEnumerator Start()
+    public Server Server;
+    public Ghost GhostPrefab;
+
+    private void Start()
     {
-        while (true)
+        var instance = Instantiate(GhostPrefab);
+
+        var readers = instance.GetComponentsInChildren<NetReader>();
+        var senders = instance.GetComponentsInChildren<NetSender>();
+
+        foreach (var c in Server.Connections)
         {
-            yield return new WaitForSeconds(1);
-            // Make Cube
-            // Make a ghost
+            var writer = new DataStreamWriter(1000, Allocator.Temp);
+            writer.Write(10); // Unique Packet ID
+            writer.Write(1);  // Prefab ID
+            writer.Write(instance.GetInstanceID());
+            Server.Write(writer, c);
         }
+
+        /*
+        foreach (var sender in senders)
+            Server.AddSender(sender);
+        */
     }
+}
 
-    // Ghost
-
+public class SpawnReader : NetReader
+{
+    public override void Read(int connectionId, DataStreamReader stream, ref DataStreamReader.Context context)
+    {
+        var prefabId = stream.ReadInt(ref context);
+        var instanceId = stream.ReadInt(ref context);
+        // Create an object from the prefab id. Collection[prefabId]
+    }
 }
