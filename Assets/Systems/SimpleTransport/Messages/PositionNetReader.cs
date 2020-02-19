a46638826d4148f8f857f7f96b2bf20c5776847f
Tin <tin@liminalvr.com>
2020-02-20 10:10:09 +1100

    Add positions too

--- Assets/Systems/SimpleTransport/Messages/PositionNetReader.cs
+++ Assets/Systems/SimpleTransport/Messages/PositionNetReader.cs
@@ -6,10 +6,15 @@ public class PositionNetReader : NetReader
     public Transform Target;
     public override void Read(int connectionId, DataStreamReader stream, ref DataStreamReader.Context context)
     {
-        var x = (float)stream.ReadInt(ref context)/1000;
+        var x = (float)stream.ReadInt(ref context)/10000;
+        var y = (float)stream.ReadInt(ref context)/10000;
+        var z = (float)stream.ReadInt(ref context)/10000;
+
         var pos = Target.position;
         pos.x = x;
-        pos.y = 0.5F;
-        Target.position = pos;
+        pos.y = y;
+        pos.z = z;
+
+        Target.position = Vector3.Lerp(Target.position, pos, 8 * Time.deltaTime);
     }
 }
--- Assets/Systems/SimpleTransport/Messages/PositionNetSender.cs
+++ Assets/Systems/SimpleTransport/Messages/PositionNetSender.cs
@@ -1,20 +1,22 @@
 ﻿using Unity.Collections;
 using Unity.Networking.Transport;
-using UnityEngine;
 
 public class PositionNetSender : NetSender
 {
-    public bool Log;
-
-    public override DataStreamWriter GetNew() { return new DataStreamWriter(12, Allocator.Temp); }
+    public override DataStreamWriter GetNew() { return new DataStreamWriter(20, Allocator.Temp); }
 
     public override DataStreamWriter Write(DataStreamWriter writer)
     {
         if (InstanceId != null)
             writer.Write(InstanceId.Value);
 
-        var val = transform.position.x * 1000;
-        writer.Write((int)val);
+        var x = transform.position.x * 10000;
+        var y = transform.position.y * 10000;
+        var z = transform.position.z * 10000;
+
+        writer.Write((int)x);
+        writer.Write((int)y);
+        writer.Write((int)z);
 
         return writer;
     }
